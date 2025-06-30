0a3f9aa0ca1a5e6121ae8af935442acd63e44e46
Darkblazealpha <ummyousuf13@gmail.com>
2025-06-30 20:11:44 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+let canvas,classifier;
+function setup() {
+  canvas=createCanvas(400, 400);
+  canvas.center();
+  background("pink");
+  classifier = ml5.imageClassifier("DoodleNet",modelLoaded);
+  canvas.mouseReleased(classifyCanvas);
+}
+
+function classifyCanvas(){
+  classifier.classify(canvas, gotResult);
+}
+
+function gotResult(results){
+  console.log(results);
+  document.getElementById('name').innerHTML=results[0].label;
+  document.getElementById('name').innerHTML=(results[0].confidence*100).toFixed(2)+ "%";
+}
+
+function modelLoaded(){
+  console.log("The model is loaded")
+}
+
+function draw() {
+  strokeWeight(13);
+  stroke("green");
+  if (mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+  }
+}
+
+function clearCanvas(){
+  background("pink")
+}
